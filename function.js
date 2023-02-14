e4a44c209d16152164421bc21d777f408e31d0b4
Augusto Garrone <garrone.agusto88@gmail.com>
2023-02-14 12:11:27 -0300

    primer escritura en el repositor

---
+++ function.js
@@ -0,0 +1,8 @@
+document.getElementById("boton").onclick = function() {
+    console.log("capturamos el elemento click");
+    document.getElementById("demo").innerHTML = "DEJA TU CONTACTO EN LA SIGUIENTE CASILLA"; 
+}
+document.getElementById("boton2").onclick = function() {
+    console.log("capturamos el elemento click");
+    document.getElementById("demo").innerHTML = "GRACIAS POR SU ATENCIÓN"; 
+}
